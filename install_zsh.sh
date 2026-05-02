170bdda2e7651aa5c2396b369a70dad0d7c941d1
wwhds <a1605691832@163.com>
2026-05-02 19:27:17 +0800

    update

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Detect the operating system and execute the corresponding script
+# 检测操作系统
 OS=$(uname -s)
 
+# 根据操作系统执行相应的安装脚本
 if [[ "$OS" == "Darwin" ]]; then
     echo "macOS detected, executing macOS installation script..."
-    bash ./install_macos.sh
+    curl -sL https://raw.githubusercontent.com/WwhdsOne/terminal-tools/main/install_macos.sh | bash
 elif [[ "$OS" == "Linux" ]]; then
     echo "Ubuntu/Debian detected, executing Linux installation script..."
-    bash ./install_ubuntu_debian.sh
+    curl -sL https://raw.githubusercontent.com/WwhdsOne/terminal-tools/main/install_ubuntu_debian.sh | bash
 else
     echo "Unsupported OS detected. Please install manually."
     exit 1
